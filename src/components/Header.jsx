65bb7b7432c249d5e28add67c5fce1f6d027874e
LOVKUSH <lovkushyadav9888@gmail.com>
2023-02-19 18:17:58 +0530

    Completing all the components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 const Header = () => {
   return (
     <nav>
-        <h1>Lovkush Yadav</h1>
-        <main>
-            <Link to = {"/"}>Home</Link>
-            <Link to = {"/"}>Contact</Link>
-            <Link to = {"/#about"}>About</Link>
-            <Link to = {"/#brands"}>Brands</Link>
-            <Link to = {"/"}>Services</Link>
-            
-        </main>
-
-
+      <h1>TechyStar.</h1>
+      <main>
+        <HashLink to={"/#home"}>Home</HashLink>
+        <Link to={"/contact"}>Contact</Link>
+        <HashLink to={"/#about"}>About</HashLink>
+        <HashLink to={"/#brands"}>Brands</HashLink>
+        <Link to={"/services"}>Services</Link>
+      </main>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
